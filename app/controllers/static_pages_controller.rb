e2026eeb3f6127e8e194a1c743cf4d7c43088743
Kin Guerra <kin.guerra@gmail.com>
2013-07-18 23:48:18 -0300

    Poems order

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,6 +2,6 @@ class StaticPagesController < ApplicationController
   def filme
   	@noticia = Post.order("created_at desc").limit(3)
   	@exibition = Cinema.order("created_at desc")
-  	@poem = Poem.order("title")
+  	@poem = Poem.order("created_at DESC")
   end
 end
