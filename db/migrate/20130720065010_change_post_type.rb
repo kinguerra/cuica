c837f6a9f8dd9fa88d20bef5f4a2060dfa5f2d37
Kin Guerra <kin.guerra@gmail.com>
2013-07-20 03:52:26 -0300

    change posts type

---
+++ db/migrate/20130720065010_change_post_type.rb
@@ -0,0 +1,9 @@
+class ChangePostType < ActiveRecord::Migration
+  def up
+  	change_column :posts, :content, :text, :limit => nil
+  end
+
+  def down
+	change_column :posts, :content, :string
+  end
+end
